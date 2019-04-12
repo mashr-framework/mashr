3d5bbe8f5c907f04a6d9f733c568fcae492ed217
Jacob Coker-Dukowitz <jacobleecd@gmail.com>
2019-04-12 10:09:31 -0700

    add init.js edits
    Co-authored-by: Mat Sachs <matochondrion@gmail.com>
    Co-authored-by: Linus Phan <phanlinus@gmail.com>
    Co-authored-by: Jacob Coker <jacobleecd@gmail.com>

--- src/commands/init.js
+++ src/commands/init.js
@@ -4,15 +4,14 @@ const ora = require('ora');
 
 module.exports = async (args) => {
   const spinner = ora().start();
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  spinner.stop();
-  // try {
-  //   const workingDir = path.resolve("./");
-  //   const destination = `${workingDir}/mashr_config.yml`;
-  //   const configTemplate = `${__dirname}/${'../../templates/mashr_template.yml'}`
-  //   await fs.copyFile(configTemplate, destination);
-  //   spinner.stop()
-  // } catch (err) {
-  //   spinner.stop()
-  // }
+
+  try {
+    const workingDir = path.resolve("./");
+    const destination = `${workingDir}/mashr_config.yml`;
+    const configTemplate = `${__dirname}/${'../../templates/mashr_template.yml'}`
+    await fs.copyFile(configTemplate, destination);
+    spinner.stop()
+  } catch (err) {
+    spinner.stop()
+  }
 }

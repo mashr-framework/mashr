0e073fa010209aa150bfd8b5805dbf128a12c5b3
Jacob Coker-Dukowitz <jacobleecd@gmail.com>
2019-04-22 20:20:37 -0700

    fixed configureCredentials to return a value
    Co-authored-by: Mat Sachs <matochondrion@gmail.com>
    Co-authored-by: Linus Phan <phanlinus@gmail.com>
    Co-authored-by: Jacob Coker <jacobleecd@gmail.com>

--- src/utils/configureCredentials.js
+++ src/utils/configureCredentials.js
@@ -9,7 +9,5 @@ module.exports = async function configureCredentials(mashrConfigObj) {
 
 const getPathToKeyFile = async (mashrConfigObj) => {
   const filename = mashrConfigObj.mashr.json_keyfile;
-  const pathname = `${path.resolve('./')}/${filename}`;
-
-  // return await validateKeyfile(pathname);
+  return `${path.resolve('./')}/${filename}`;
 };

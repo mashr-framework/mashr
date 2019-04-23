d64c8447f8945f2795b49edb976bf71aa8a8928a
Jacob Coker-Dukowitz <jacobleecd@gmail.com>
2019-04-23 12:26:37 -0700

    added detail to mashr templates; updated Dockerfile to include embulk-formatter-jsonl
    Co-authored-by: Mat Sachs <matochondrion@gmail.com>
    Co-authored-by: Linus Phan <phanlinus@gmail.com>
    Co-authored-by: Jacob Coker <jacobleecd@gmail.com>

--- src/commands/list.js
+++ src/commands/list.js
@@ -4,6 +4,13 @@ module.exports = async (args) => {
   const infoObj = await readResources();
   const integrationNames = Object.keys(infoObj.integrations);
 
+  if (integrationNames.length === 0) {
+    console.log(`
+      No integrations. Run "mashr init" to start a new` +
+     ` integration.
+     `);
+    return;
+  }
   const result = integrationNames.map(function (name) {
     var integration = infoObj.integrations[name];
 

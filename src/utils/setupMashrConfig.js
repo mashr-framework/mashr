eec4534353df780abd2878c52543e27dcd79ad1e
matochondrion <matochondrion@gmail.com>
2019-04-24 22:33:50 +0000

    removed unused files
    Co-authored-by: Mat Sachs <matochondrion@gmail.com>
    Co-authored-by: Linus Phan <phanlinus@gmail.com>
    Co-authored-by: Jacob Coker <jacobleecd@gmail.com>

--- src/utils/setupMashrConfig.js
+++
@@ -1,7 +0,0 @@
-// const copyMashrConfigTemplate = require('./copyMashrConfigTemplate');
-// // const addServiceAccountEmail = require('./addServiceAccountEmail');
-
-// module.exports = async function setupMashrConfig(serviceAccount) {
-//   await copyMashrConfigTemplate();
-//   // await addServiceAccountEmail(serviceAccount);
-// }

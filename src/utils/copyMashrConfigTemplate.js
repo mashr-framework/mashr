4e1a2e2b2477355865592d7985aa1e509481aafd
Linus Phan <phanlinus@gmail.com>
2019-04-23 19:32:27 -0700

    move spinner instantiation
    Co-authored-by: Mat Sachs <matochondrion@gmail.com>
    Co-authored-by: Linus Phan <phanlinus@gmail.com>
    Co-authored-by: Jacob Coker <jacobleecd@gmail.com>

--- src/utils/copyMashrConfigTemplate.js
+++ src/utils/copyMashrConfigTemplate.js
@@ -3,12 +3,11 @@ const { copyFile } = require('../utils/fileUtils');
 const ora = require('ora');
 const mashrLogger = require('../utils/mashrLogger');
 
-const spinner = ora();
-
 const workingDir = path.resolve('./');
 const destination = `${workingDir}/mashr_config.yml`;
 
 module.exports = async function copyMashrConfigTemplate() {
+  const spinner = ora();
   const configTemplate = `${__dirname}/${'../../templates/mashr_template.yml'}`
 
   await copyFile(configTemplate, destination);
